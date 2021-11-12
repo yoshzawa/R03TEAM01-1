d6d5cbf1e867b7a81556848b3e2ebb9561ff7ef5
yoshzawa <yoshzawa@gmail.com>
2021-11-12 16:29:02 +0900

    プロジェクト ファイルを追加します。

---
+++ App3/page/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App3/page/MainPage1.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace App3.page
+
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
