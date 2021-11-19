700c09bf3e320f429994db1b3c013d7ed431056b
yoshzawa <yoshzawa@gmail.com>
2021-11-19 11:43:08 +0900

    わからないけど動くかな？

--- App3/page/MainPage1.xaml.cs
+++ App3/page/MainPage1.xaml.cs
@@ -1,4 +1,12 @@
 ﻿using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace App3.page
 
@@ -8,6 +16,36 @@ namespace App3.page
         public MainPage1()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+             {
+                 await CrossMedia.Current.Initialize();
+
+                 if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                 {
+                     await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                     return;
+                 }
+
+                 var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                 {
+                     Directory = "Sample",
+                     Name = "test.jpg"
+                 });
+
+                 if (file == null)
+                     return;
+
+                 await DisplayAlert("File Location", file.Path, "OK");
+
+                 image.Source = ImageSource.FromStream(() =>
+                 {
+                     var stream = file.GetStream();
+                     return stream;
+                 });
+             };
         }
     }
 }
