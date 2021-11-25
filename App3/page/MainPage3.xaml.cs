eab478dc6844448c2513d06dd91379deee178999
yoshzawa <yoshzawa@gmail.com>
2021-11-25 16:19:00 +0900

    WebViewを追加してみた

---
+++ App3/page/MainPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3.page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage3 : ContentPage
+    {
+        public MainPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
